c877852321a3d59975eab21d105f34d2e0754a3c
Guusy <guusygonzalo@gmail.com>
2018-05-21 23:23:46 -0300

    smoke test with db.json

---
+++ cypress/integration/smoke-test.spec.js
@@ -0,0 +1,52 @@
+describe('Smoke Tests',()=>{
+
+    beforeEach(()=>{
+        cy.request('DELETE','/api/todos/all')
+    })
+    context('No todos',()=>{
+        it('Add new todo',()=>{
+            cy.server()
+            cy.route('POST','/api/todos').as('save')
+
+            cy.visit('/')
+
+            cy.get('.new-todo').type('New Todo{enter}')
+
+            cy.wait('@save')
+
+
+            cy.get('.todo-list li')
+            .should('have.length',1)
+
+        })
+    })
+    context('With todos',()=>{
+        beforeEach(()=>{
+            cy.fixture('todos')
+            .then(todos=>{
+                cy.request('POST','/api/todos/bulkload',{todos})
+            })
+
+            cy.server()
+            cy.route('GET','/api/todos').as('load')
+
+            cy.visit('/')
+
+            cy.wait('@load')
+        })
+
+        it.only('Delete todos',()=>{
+            cy.route('DELETE','/api/todos/*').as('delete')
+            cy.get('.todo-list li').each(element =>{
+                cy.wrap(element)
+                .find('.destroy')
+                .invoke('show')
+                .click()
+
+
+                cy.wait('@delete')
+            }).should('not.exist')
+        })
+
+    })
+})
